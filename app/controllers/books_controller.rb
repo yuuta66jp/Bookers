06c9f629bd378c4bc14e60e54a9a3d858c60e8dd
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-01-12 14:21:01 +0900

    [Finish]ルーティング、コントローラ設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,46 @@
 class BooksController < ApplicationController
+
   def top
   end
+
+  def new
+  	  @book = Book.new
+  end
+
+  def create
+  	  book = Book.new(book_params)
+  	  book.save
+  	  redirect_to book_path(book.id)
+  end
+
+  def index
+  	  @books = Book.all
+  end
+
+  def show
+  	  @book = Book.find(params[:id])
+  end
+
+  def edit
+  	  @book = Book.find(params[:id])
+  end
+
+  def update
+  	  book = Book.find(params[:id])
+  	  book.update(book_params)
+  	  redirect_to book_path(book.id)
+  end
+
+  def destroy
+  	  book = Book.find(params[:id])
+  	  book.destroy
+  	  redirect_to books_path
+  end
+
+private
+
+  def book_params
+  	  params.require(:book).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
   root :to => 'books#top'
 
   get 'books/new'
+
+  post 'books' => 'books#create'
+
+  get 'books' => 'books#index'
+
+  get 'books/:id' => 'books#show', as: 'book'
+
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+
+  patch 'books/:id' => 'books#update', as: 'update_book'
+
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+
 end
